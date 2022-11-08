319eda25f42fa107304545a1c305b9be8665b266
AbdullahRedoan27 <abdullahredoan8@gmail.com>
2022-11-08 22:08:18 +0600

    services

---
+++ api.js
@@ -0,0 +1,56 @@
+[
+    {
+        "name": "Article Writing",
+        "price": 15,
+        "ratings": 4.5,
+        "ratingsCount": 3725,
+        "img": "https://i.ibb.co/826q9cv/Articles-Writing.png",
+        "description":"Creative writing style makes an article far better. You will find the most creative and qualityfull article writing service here. Articles would be written on the topic provided. The article would be ready to publish in any newspaper or journal",
+        "points":["Creative Writing", "Ready to be published", "Up to 3 times revision survice", "Fast delivary"]
+      },
+    {
+        "name": "Content Editing",
+        "price": 15,
+        "ratings": 5,
+        "ratingsCount": 3500,
+        "img": "https://i.ibb.co/nrSxKPj/Content-Editing.png",
+        "description":"Most of the time content quality drops because of it's faulty script. We are here to solve this problem. You can make your content better by recheking and editing your script. We will recheck and edit your content.",
+        "points":["Most correct and attractive content management", "Top quality service assured", "Up to 3 times revision survice", "Fast delivary"]
+      },
+    {
+        "name": "Speech Writing",
+        "price": 20,
+        "ratings": 4.5,
+        "ratingsCount": 3000,
+        "img": "https://i.ibb.co/gDLZb0J/Speech-Writing.png",
+        "description":"A good speech can make your reputation much higher whether a bad speech can ruin your reputation. That's why a good speech is very important in public interactions. We will provide you the best speech script according to your provided subject.",
+        "points":["Attractive and elegent words used", "Simple and understandable", "Up to 3 times revision survice", "Fast delivary"]
+    },
+    {
+        "name": "Research",
+        "price": 100,
+        "ratings": 4.5,
+        "ratingsCount": 3204,
+        "img": "https://i.ibb.co/s2gL3b6/Research.png",
+        "description":"Research is very important but people make mistakes with this. But we are here to make your work so easy. You can provide us topics and we will make some research work on them. We can submit the final work in any file type as per your wish.",
+        "points":["Attractive and elegent words used", "Simple and understandable", "Up to 3 times revision survice", "Fast delivary"]
+    },
+    {
+        "name": "Brand Promotion",
+        "price": 200,
+        "ratings": 4.5,
+        "ratingsCount": 3700,
+        "img": "https://i.ibb.co/wSZx9yJ/Brand-Promotion.png",
+        "description":"Now a days, no products can generate sell without proper promotion. If you try to promote your product by your own, it will be costlier and you will not find any gurantee about the work. But if you hire us, we will make your work easier, cost efficient and guranteed.",
+        "points":["Provide appropriate information", "Increase brand awareness", "Increase Customer Traffic", "Fast delivary"]
+    },
+    {
+        "name": "News Script Writing",
+        "price": 20,
+        "ratings": 5,
+        "ratingsCount": 3000,
+        "img": "https://i.ibb.co/TqdjDBb/news.png",
+        "description":"People now don't read newspaper that much. It is now the era of video footage. We can write some elegant, attarctive script for your news channel. The channel can be on youtube or satelite. We can gurantee you about our scripts.",
+        "points":["Attractive and elegent words used", "Simple and understandable", "Up to 3 times revision survice", "Fast delivary"]
+    },
+]
--- index.js
+++ index.js
@@ -1,10 +1,55 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
+require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.zpaqsgt.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+
+    try{
+        const servicesCollection = client.db('PiJournal').collection('services');
+        const reviewCollection = client.db('PiJournal').collection('reviews')
+
+        app.get('/services', async(req, res)=>{
+            const query = {};
+            const cursor = servicesCollection.find(query);
+            const services =await cursor.toArray();
+            res.send(services);
+        })
+        app.get('/services/:id', async(req, res)=>{
+            const id = req.params.id
+            const query = {_id: ObjectId(id)}
+            const result =await servicesCollection.findOne(query);
+            res.send(result);
+        })
+        app.get('/reviews', async(req, res) => {
+            const query = {}
+            const cursor = reviewCollection.find(query);
+            const reviews = await cursor.toArray();
+            res.send(reviews);
+        })
+
+        app.post('/reviews', async(req, res)=>{
+            const review = req.body;
+            console.log(review);
+            const result = await reviewCollection.insertOne(review);
+            res.send(result);
+        })
+    }
+    finally{
+
+    }
+}
+run().catch(error => console.error(error))
 
 app.get('/', (req, res)=>{
     res.send('pijournal server is running')
